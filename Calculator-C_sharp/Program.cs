729d90fe3c4dd7b6f1a2079d5833408d05d10ea7
Nursultan Yerzhan <nursultan_erzhan@mail.ru>
2021-02-06 18:07:03 +0600

    +1

--- Calculator-C_sharp/Program.cs
+++ Calculator-C_sharp/Program.cs
@@ -7,9 +7,6 @@ namespace Calculator_C_sharp
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
